ea5393ee4d7dd852e224ddec52dc0f3dd748f22e
Serdar Gorumlu <sgorumlu@gmail.com>
2024-03-07 12:59:47 -0600

    Loading Skeleton added for Login

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,6 +14,7 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import Skeleton from "@/app/components/Skeleton";
 
 const NavBar = () => {
   return (
@@ -62,7 +63,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width={"3rem"} />;
 
   if (status === "unauthenticated")
     return (
