e173c242ab9f9812d8113b46b4b69f649556f265
Serdar Gorumlu <sgorumlu@gmail.com>
2024-03-07 12:12:39 -0600

    Add a drop-down menu to show current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,16 @@ import { AiFillBug } from "react-icons/ai";
 import clasnames from "classnames";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -43,7 +52,21 @@ const NavBar = () => {
             </ul>
           </Flex>
           <Box>
-            {status === "authenticated" && <Link href={"/api/auth/signout"}>Log Out</Link>}{" "}
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar src={session.user!.image!} fallback="?" size={"2"} radius="full" className="cursor-pointer" />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenuLabel>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    <Link href={"/api/auth/signout"}>Log Out</Link>
+                  </DropdownMenuItem>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
             {status === "unauthenticated" && <Link href={"/api/auth/signin"}>Login</Link>}{" "}
           </Box>
         </Flex>
