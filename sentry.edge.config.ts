01f52be64ade1ef75ba189786ff6c5ed487d25bc
Serdar Gorumlu <sgorumlu@gmail.com>
2024-03-09 00:08:41 -0600

    error tracking setup using sentry

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4fc38a976c0366aa20bbc1298d6c42bb@o4506879439536128.ingest.us.sentry.io/4506879443402752",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
